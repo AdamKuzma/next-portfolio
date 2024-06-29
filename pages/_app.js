f9662903813deac5be02e56306117d2bb4d0532a
adamkuzma <adam@esecure.cc>
2024-06-28 21:01:31 -0400

    Font fix

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preload" href="../fonts/PPMondwest-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="preload" href="../fonts/Inter.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
       </Head>
       <Nav />
       <Component {...pageProps} />
