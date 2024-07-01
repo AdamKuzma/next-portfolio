fb34fcfbf96b4d1d0280e113b22f4631668492cf
adamkuzma <adam@esecure.cc>
2024-07-01 18:37:37 -0400

    Mobile down

--- pages/projects/amor-components.js
+++ pages/projects/amor-components.js
@@ -35,29 +35,30 @@ function AmorComponents() {
             My role involved developing a set of reusable content components in Figma and Hybris (our CMS) that encompassed marketing materials with supporting documentation, leveraging the visual language established in the Amor design system.
         </p>
 
-        <p className='mb-8 lg:w-3/5 fade-in delay-4'>
+        <p className='mb-12 lg:w-3/5 fade-in delay-4'>
             The redesigned CMS tool, improved the efficiency of our marketing team’s content management processes, currently with over 1,000 components in use across our websites.
         </p>
 
-        <div className='flex mb-24 mt-24 rounded-xl'>
-            <video className='rounded-xl' ref={el => videoRefs.current[0] = el} muted autoplay loop playsInline>
+        <div className='flex mb-3 py-8 px-8 lg:py-12 lg:px-12 bg-neutral-100 fade-in delay-5 rounded-lg'>
+            <video className='rounded-md' ref={el => videoRefs.current[0] = el} muted autoplay loop playsInline>
                 <source type='video/mp4' src='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FAmor-Vid.mp4?alt=media&token=36b16b98-208c-4c60-baba-b29152c3b6c6'></source>
             </video>
         </div>
+        <p className='mb-12 annotation text-sm'>Speed of creating a page using new components</p>
 
 
         {/* Leveraging Design System */}
 
         <b className='mb-4 block'>Leveraging Design System</b>
-        <p className='mb-8 lg:w-3/5'>
+        <p className='mb-4 lg:w-3/5'>
             An audit of Signet’s content pages was necessary from both design and engineering perspective. We used 68 different fonts and 41 colors across five websites at the time, which was a severe performance concern.
         </p>
 
-        <p className='mb-8 lg:w-3/5'>
+        <p className='mb-12 lg:w-3/5'>
             When building the new components, I started to standardize our use of color and typography, extending the visual language defined in our design system. This effort helped content authors create new marketing material at velocity and maintain a consistent brand experience.
         </p>
 
-        <div className='mb-14 mt-14 py-12 px-12 bg-gray-100 rounded-xl'>
+        <div className='mb-12 py-6 px-4 lg:py-12 lg:px-12 bg-neutral-100 rounded-lg'>
             <img className='' src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FAmor-Exp-Design-System.png?alt=media&token=25728fc3-2886-4869-a75a-3dc056bd4ca0" alt="Amor Components" />
         </div>
 
@@ -65,34 +66,31 @@ function AmorComponents() {
         {/* Component Types */}
 
         <b className='mb-4 block'>Component Types</b>
-        <p className='mb-8 lg:w-3/5'>
+        <p className='mb-12 lg:w-3/5'>
             Documenting content patterns found across hundreds of pages helped define the scope for the initial batch of components. The new components standardized the most commonly used content patterns and provided better guidance based on the intent behind adding content. These components set the groundwork for creating page templates.
         </p>
 
-        <p className='mb-8 lg:w-3/5'>
-            Individual brand expression is implemented in each component via typography, language, color, and imagery.
-        </p>
-
-        <div className='mb-14 mt-14 py-12 px-12 bg-gray-100 rounded-xl'>
+        <div className='mb-12 py-6 px-4 lg:py-12 lg:px-12 bg-neutral-100 rounded-lg'>
             <img className='' src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FExp-Component-Types.png?alt=media&token=cf10a468-afb7-454b-a538-f0414d87f8a2" alt="Amor Components" />
         </div>
 
-        <div className='flex mb-24 mt-12 rounded-xl'>
-            <video className='rounded-xl' ref={el => videoRefs.current[1] = el} muted autoplay loop playsInline>
+        <div className='flex mb-3 py-4 bg-[#1A1A1A] rounded-lg'>
+            <video className='rounded-lg' ref={el => videoRefs.current[1] = el} muted autoplay loop playsInline>
                 <source type='video/mp4' src='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FBanner-Variations.mp4?alt=media&token=0f8950c9-019f-471e-a845-0760b5eb2592'></source>
             </video>
         </div>
+        <p className='mb-12 annotation text-sm'>Individual brand expression implemented via typography, language, color, and imagery</p>
 
 
         {/* Responsive Design */}
 
         <b className='mb-4 block'>Responsive Design</b>
-        <p className='mb-8 lg:w-3/5'>
-            One of the marketing team’s biggest pain points was ensuring the page designs were displaying correctly on all devices. In the past, content authors designed adaptive pages relying primarily on CSS media queries, requiring a separate desktop and mobile code. The new components eliminated the struggle of developing for different devices by having a pre-defined responsive behavior.
+        <p className='mb-12 lg:w-3/5'>
+            In the past, content authors designed adaptive pages relying primarily on CSS media queries, requiring a separate desktop and mobile code. The new components eliminated the struggle of developing for different devices by having a pre-defined responsive behavior.
         </p>
 
-        <div className='flex mb-24 mt-12 rounded-xl'>
-            <video className='rounded-xl' ref={el => videoRefs.current[2] = el} muted autoplay loop playsInline>
+        <div className='flex mb-12 rounded-lg'>
+            <video className='rounded-lg' ref={el => videoRefs.current[2] = el} muted autoplay loop playsInline>
                 <source type='video/mp4' src='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FResponsive-Amor-2.mp4?alt=media&token=143ebad0-425c-4592-ae14-efcff99bd381'></source>
             </video>
         </div>
@@ -101,12 +99,12 @@ function AmorComponents() {
         {/* Dynamic Properties */}
 
         <b className='mb-4 block'>Dynamic Properties</b>
-        <p className='mb-8 lg:w-3/5'>
-            Dynamic properties offer flexibility within a system. Content authors can choose different properties within a component or toggle elements on and off. This functionality was embedded both within Figma and Hybris, helping us maintain simplicity and clear content hierarchy.
+        <p className='mb-12 lg:w-3/5'>
+            Content authors can choose different properties within a component or toggle elements on and off. This functionality was embedded both within Figma and Hybris, helping us maintain simplicity and clear content hierarchy.
         </p>
 
-        <div className='flex mb-24 mt-12 rounded-xl'>
-            <video className='rounded-xl' ref={el => videoRefs.current[3] = el} muted autoplay loop playsInline>
+        <div className='flex mb-12 rounded-lg'>
+            <video className='rounded-lg' ref={el => videoRefs.current[3] = el} muted autoplay loop playsInline>
                 <source type='video/mp4' src='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FAmor-Properties.mp4?alt=media&token=75887917-2cc2-40f8-9c83-c4300b63f49b'></source>
             </video>
         </div>
@@ -115,18 +113,18 @@ function AmorComponents() {
         {/* Component Documentation */}
 
         <b className='mb-4 block'>Component Documentation</b>
-        <p className='mb-8 lg:w-3/5'>
+        <p className='mb-4 lg:w-3/5'>
             Collaborating with developers, I created detailed documentation for each component around its typography, spacing, colors, dimensions, and character limits. Keyboard navigation for carousels, header hierarchy, and a space to add alt text in Hybris were also part of the considerations for accessible design.
         </p>
-        <p className='mb-8 lg:w-3/5'>
+        <p className='mb-12 lg:w-3/5'>
             Specifications on usage and examples helped designers and the marketing team understand how to start using components. Amor Content Components are a part of our Figma library, and anyone can access their documentation in ZeroHeight.
         </p>
 
-        <div className='mb-14 mt-14 py-12 px-12 bg-gray-100 rounded-xl'>
+        <div className='mb-14 py-8 px-4 lg:py-12 lg:px-12 bg-neutral-900 rounded-lg'>
             <img className='' src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FType-Specs.png?alt=media&token=19561d95-fe34-4697-98f8-4b8fdfcded0f" alt="Amor Components" />
         </div>
 
-        <div className='mb-14 mt-14 py-12 px-12 bg-gray-100 rounded-xl'>
+        <div className='mb-14 py-8 px-4 lg:py-12 lg:px-12 bg-neutral-900 rounded-lg'>
             <img className='' src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FSpacing-Specs.png?alt=media&token=c752c4c3-423d-44c1-84f9-ba2fb3d99d95" alt="Amor Components" />
         </div>
 
@@ -135,10 +133,7 @@ function AmorComponents() {
 
         <b className='mb-4 block'>Empowering Contributors</b>
         <p className='mb-8 lg:w-3/5'>
-            Design Systems are only successful when they can be easily used by anyone working on the product. For this project, I partnered with our design system team to develop guidelines and training materials tailored to user types and knowledge levels, advocating for the adoption, maintenance, and upkeep of components and documentation. We developed an intake process to submit new component requests or updates to support contributions from our designers and marketing teams.
-        </p>
-        <p className='mb-8 lg:w-3/5'>
-            Amor Content Components have empowered everyone in the organization to launch new marketing pages rapidly and consistently, improving our website’s interface with confidence.
+            Design Systems are only successful when they can be easily used by anyone working on the product. For this project, I partnered with our design system team to develop guidelines and training materials, advocating for the adoption, maintenance, and upkeep of components and documentation. We developed an intake process to submit new component requests or updates to support contributions from our designers and marketing teams.
         </p>
 
 
@@ -146,7 +141,7 @@ function AmorComponents() {
 
         <b className='mb-4 block'>Impact</b>
         <p className='mb-8 lg:w-3/5'>
-            Impact included here
+            Amor Content Components have empowered everyone in the organization to launch new marketing pages rapidly and consistently, improving our website’s interface with confidence. Two years after their release, we've have recorded over 3000 component instances used across our websites.
         </p>
 
       <SayHi></SayHi>
--- pages/projects/member-rewards.js
+++ pages/projects/member-rewards.js
@@ -28,15 +28,15 @@ function MemberRewards() {
 
         {/* Project Intro */}
 
-        <p className='mb-8 fade-in delay-2 lg:w-3/5'>
+        <p className='mb-4 lg:w-3/5 fade-in delay-2 '>
             The introduction of the Member Rewards initiative marks a significant enhancement to our loyalty program, allowing members to earn shopping credits. These credits can be applied to their purchase, complementing existing promo codes and fostering a more rewarding shopping experience.
         </p>
 
-        <p className='mb-8 fade-in delay-3 lg:w-3/5'>
+        <p className='mb-12 lg:w-3/5 fade-in delay-3'>
             I conducted an audit of our apps, mapping all of the styles, spacings, treatments, and components we were using. Next, I developed a comprehensive system that was minimal enough to converge all the inconsistencies into fewer styles and components, while still being simple to use for our designers.
         </p>
 
-      <div className='flex gap-10 mb-14 mt-14 py-12 px-12 bg-neutral-100 rounded-xl'>
+      <div className='flex gap-4 lg:gap-10 mb-12 py-6 px-4 lg:py-12 lg:px-12 bg-neutral-100 rounded-lg fade-in delay-4'>
         <div className='flex-1'>
           <img className='w-full h-auto' src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FScreen-1-Member.png?alt=media&token=981ba837-bfcb-4635-b821-b75a3f367600" alt="Digital Store System" />
         </div>
@@ -48,18 +48,18 @@ function MemberRewards() {
           </div>
       </div>
 
-      <div className='mb-14 py-12 px-12 bg-neutral-100 rounded-xl'>
+      <div className='mb-12 py-6 px-4 lg:py-12 lg:px-12 bg-neutral-100 rounded-lg'>
         <img className='' src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2F000.png?alt=media&token=746f9932-13fa-4590-80dc-f6840b8d81c4" alt="Digital Store System" />
       </div>
 
       {/* Gathering Requirements */}
 
       <b className='mb-4 block'>Gathering the Requirements</b>
-      <p className='mb-8 lg:w-3/5 fade-in delay-2'>
+      <p className='mb-12 lg:w-3/5 fade-in delay-2'>
         We started with a comprehensive analysis of the new program’s requirements. Engaging with cross-functional teams, we crafted a detailed project brief, identified potential challenges, and outlined key design considerations.
       </p>
 
-      <div className='mb-14 mt-14 py-12 px-12 bg-neutral-100 rounded-xl'>
+      <div className='mb-12 py-6 px-4 lg:py-12 lg:px-12 bg-neutral-100 rounded-lg'>
         <img className='invert' src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FRoadmapC.svg?alt=media&token=510bf371-74bd-424b-bfb5-d36a63014d2e" alt="Digital Store System" />
       </div>
 
@@ -67,31 +67,31 @@ function MemberRewards() {
       {/* Structuring the Experience */}
 
       <b className='mb-4 block'>Structuring the Experience</b>
-      <p className='mb-8 lg:w-3/5 fade-in delay-2'>
+      <p className='mb-4 lg:w-3/5 fade-in delay-2'>
         To make sense of the complex project requirements, I started exploring the potential user flows considering all the possible scenarios that depend on different variables.
       </p>
-      <p className='mb-8 lg:w-3/5 fade-in delay-2'>
+      <p className='mb-12 lg:w-3/5 fade-in delay-2'>
         Factors such as membership status, authenticated state, exclusions, order total, items in the bag, applied promos, promo hierarchy, and rewards expiration would all result in a different experience for the user.
       </p>
 
-      <div className='mb-14 mt-14 py-12 px-12 bg-neutral-100 rounded-xl'>
+      <div className='mb-14 py-12 px-4 lg:py-12 lg:px-12 bg-neutral-100 rounded-lg'>
         <img className='' src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FLogic-Flow-Light.png?alt=media&token=78edd8dd-0ed7-4872-b53e-0e5c3df1fbb8" alt="Digital Store System" />
       </div>
 
       {/* Exploring Ideas */}
 
       <b className='mb-4 block'>Exploring Ideas</b>
-      <p className='mb-8 lg:w-3/5 fade-in delay-2'>
+      <p className='mb-4 lg:w-3/5 fade-in delay-2'>
         Despite all the internal complexity, I wanted to create an experience as simple as possible for the end user, minimizing the friction before proceeding to checkout.
       </p>
-      <p className='mb-8 lg:w-3/5 fade-in delay-2'>
+      <p className='mb-4 lg:w-3/5 fade-in delay-2'>
         I explored different ideas through the lens of previously identified scenarios, highlighting the pros and cons of the user experience and technical debt.
       </p>
-      <p className='mb-8 lg:w-3/5 fade-in delay-2'>
+      <p className='mb-12 lg:w-3/5 fade-in delay-2'>
         With a tight timeline for the MVP release, I needed to design for an easy implementation. Some of the best user experiences required complex calculations and were not feasible in the short term, so we decided to put them out for the future and settle with alternative solutions.
       </p>
 
-      <div className='flex gap-10 mb-14 mt-14 py-12 px-12 bg-neutral-100 rounded-xl'>
+      <div className='flex flex-col lg:flex-row gap-16 lg:gap-10 mb-12 py-12 px-6 lg:py-12 lg:px-12 bg-neutral-100 rounded-lg'>
         <div className='flex-1'>
           <img className='w-full h-auto' src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FExploring-1-1200x2206.png?alt=media&token=29a44ac6-2fb4-4c5e-bf13-716ffbbcc4cc" alt="Digital Store System" />
         </div>
@@ -106,14 +106,14 @@ function MemberRewards() {
 
       {/* Applying Rewards */}
 
-      <div className='lg:flex lg:gap-16 mb-14 mt-14 '>
+      <div className='lg:flex lg:gap-16 mb-14'>
         <div className='lg:w-1/2'>
             <b className='mb-4 block'>Selecting Rewards</b>
-            <p className='mb-8 fade-in delay-2'>
+            <p className='mb-12'>
                 Customers with multiple promos available can either choose the best combination of promos and Rewards shown up front or make their selection within a drawer. It was important to visually communicate that only one promo code can be applied per order, while the Rewards can be redeemed in increments of one dollar.
             </p>
         </div>
-        <div className='lg:w-1/2 py-12 px-24 bg-neutral-100 rounded-xl'>
+        <div className='lg:w-1/2 py-8 px-12 lg:py-12 lg:px-24 bg-neutral-100 rounded-lg'>
             <video className='rounded-[36px]' ref={el => videoRefs.current[0] = el} muted autoplay loop playsInline>
                 <source type='video/mp4' src='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FApply-Rewards-cut.mp4?alt=media&token=92a8076f-bc61-42bd-b2e7-54539440ef57'></source>
             </video>
@@ -123,14 +123,14 @@ function MemberRewards() {
 
       {/* Selecting Rewards */}
 
-      <div className='lg:flex lg:gap-20 mb-14 mt-14 '>
+      <div className='lg:flex lg:gap-20 mb-12'>
         <div className='lg:w-1/2'>
             <b className='mb-4 block'>Applying Rewards</b>
-            <p className='mb-8fade-in delay-2'>
+            <p className='mb-12'>
                 In the final experience, loyalty members can easily view their most valuable promos and Rewards based on the items in their shopping bag. Toggling a switch applies the Rewards with all the discounts reflected under the Order Total.
             </p>
         </div>
-        <div className='lg:w-1/2 py-12 px-24 bg-neutral-100 rounded-xl'>
+        <div className='lg:w-1/2 py-8 px-12 lg:py-12 lg:px-24 bg-neutral-100 rounded-lg'>
             <video className='rounded-[36px]' ref={el => videoRefs.current[1] = el} muted autoplay loop playsInline>
                 <source type='video/mp4' src='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FSelecting-Rewards-cut.mp4?alt=media&token=ca90bad7-dbdc-4898-8861-acfe850f8bf5'></source>
             </video>
@@ -141,19 +141,19 @@ function MemberRewards() {
       {/* Potential Scenarios */}
 
       <b className='mb-4 block'>Potential Scenarios</b>
-      <p className='mb-8 lg:w-3/5 fade-in delay-2'>
+      <p className='mb-12 lg:w-3/5 fade-in delay-2'>
         Collaborating closely with dev and product partners, I identified some of the edge cases that needed to be addressed in the design, such as considerations on what happens if the customer tries to override a promo code or if their Rewards reduce the order total below zero.
       </p>
 
-      <div className='mb-14 mt-14 py-12 px-12 bg-neutral-100 rounded-xl'>
+      <div className='mb-12 py-12 px-6 lg:py-12 lg:px-12 bg-neutral-100 rounded-lg'>
         <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FScenarios-1-Export.png?alt=media&token=8f708dde-afcc-40ce-af68-b19aa1dee21e" alt="Digital Store System" />
       </div>
 
-      <div className='mb-14 py-12 px-12 bg-neutral-100 rounded-xl'>
+      <div className='mb-12 py-12 px-0 lg:py-12 lg:px-12 bg-neutral-100 rounded-lg'>
         <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FScenarios-2-Rewards.png?alt=media&token=510123ea-455f-464c-851f-a8b9941c09fc" alt="Digital Store System" />
       </div>
 
-      <div className='mb-14 py-12 px-12 bg-neutral-100 rounded-xl'>
+      <div className='mb-12 py-12 px-6 lg:py-12 lg:px-12 bg-neutral-100 rounded-lg'>
         <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/projects%2FRewards-Messages.png?alt=media&token=a1e75d10-678d-4366-920c-a94c76fd154c" alt="Digital Store System" />
       </div>
 
@@ -161,13 +161,13 @@ function MemberRewards() {
       {/* Project Takeaways */}
 
       <b className='mb-4 block'>Project Takeaways</b>
-      <p className='mb-8 lg:w-3/5 fade-in delay-2'>
+      <p className='mb-4 lg:w-3/5'>
         This project was a valuable demonstration of the advantages of involving cross-functional teams strategically early in the process.
       </p>
-      <p className='mb-8 lg:w-3/5 fade-in delay-2'>
+      <p className='mb-4 lg:w-3/5'>
         Dialogue with the program team allowed us to fill the gaps in the program requirements identified through design exploration. Collaboration with the development team helped us find a common ground to translate the tech constraints to create a user-friendly experience.
       </p>
-      <p className='mb-8 lg:w-3/5 fade-in delay-2'>
+      <p className='mb-12 lg:w-3/5'>
         While keeping the technical limitations top of mind presented a challenge in simplifying all the complexity, the additional effort invested to outline all the requirements was worthwhile, saving us a lot of time before committing to design.
       </p>
 
