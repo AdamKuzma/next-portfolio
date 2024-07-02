df3f117e3fbb9760f721a961a90a021d17bae5e9
adamkuzma <adam@esecure.cc>
2024-07-02 17:23:55 -0400

    Video Fix

--- pages/projects/clienteling-filters.js
+++ pages/projects/clienteling-filters.js
@@ -12,12 +12,12 @@ function DigitalStoreSystem() {
           console.error('Error attempting to play', error);
         });
       };
-  
+
       videoRefs.current.forEach(video => {
         if (video) {
           // Try to play the video directly
           playVideo(video);
-  
+
           // Add event listener to try to play the video on user interaction
           video.addEventListener('click', () => playVideo(video));
           video.addEventListener('touchstart', () => playVideo(video));
@@ -145,8 +145,15 @@ function DigitalStoreSystem() {
 
       {/* Contact Methods */}
 
+      <div className='mb-3 pt-8 pb- px-6 lg:pt-16 lg:pb-0 lg:px-32 bg-[#F2F2F4] rounded-lg'>
+            <video className='lazyload' ref={el => videoRefs.current[3] = el} muted autoplay loop playsInline preload="auto" poster='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/clienteling-filters%2FCL%20-%20Filter.jpg?alt=media&token=5261857a-c22e-484c-a077-1fd95bbcd81f'>
+                <source
+                  type='video/mp4'
+                  src='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/clienteling-filters%2FCL%20-%20Filter.mp4?alt=media&token=86eb82ba-8168-44c4-a933-cf777fba8cdd'></source>
+            </video>
+      </div>
       <div className='mb-3 pt-0 pb-8 px-6 lg:pt-0 lg:pb-16 lg:px-32 bg-[#F2F2F4] rounded-lg'>
-            <video className='lazyload' ref={el => videoRefs.current[3] = el} muted autoplay loop playsInline preload="auto" poster='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/clienteling-filters%2FCL%20-%20ScheduleTaskComp.jpg?alt=media&token=090c7063-8f0f-4c8c-968d-399901a262e8'>
+            <video className='lazyload' ref={el => videoRefs.current[4] = el} muted autoplay loop playsInline preload="auto" poster='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/clienteling-filters%2FCL%20-%20ScheduleTaskComp.jpg?alt=media&token=090c7063-8f0f-4c8c-968d-399901a262e8'>
                 <source
                   type='video/mp4'
                   src='https://firebasestorage.googleapis.com/v0/b/portfolio-d1c10.appspot.com/o/clienteling-filters%2FCL%20-%20Schedule%20Task.mp4?alt=media&token=d5508d0b-bbf0-4f67-a9cf-f8e9fb9528d4'></source>
