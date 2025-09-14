273eb33c553f95ca7dfa98f1c8cb0c4a5c16e319
adamkuzma <adam@esecure.cc>
2025-09-14 08:47:12 -0400

    About updateContext

--- pages/contact.js
+++ pages/contact.js
@@ -8,9 +8,8 @@ const Contact = () => (
         <p className='fade-in delay-1'><a href='mailto:adam@esecure.cc' target='_blank' rel='noopener noreferrer'>Email</a></p>
         <p className='fade-in delay-2'><a href='https://x.com/admakzm' target='_blank' rel='noopener noreferrer'>Twitter</a></p>
         <p className='fade-in delay-3'><a href='https://layers.to/admakuzma' target='_blank' rel='noopener noreferrer'>Layers</a></p>
-        <p className='fade-in delay-4'><a href='https://read.cv/adamk' target='_blank' rel='noopener noreferrer'>Read.cv</a></p>
-        <p className='fade-in delay-5'><a href='https://github.com/AdamKuzma' target='_blank' rel='noopener noreferrer'>GitHub</a></p>
-        <p className='fade-in delay-6'><a href='https://www.linkedin.com/in/adamkuzma/' target='_blank' rel='noopener noreferrer'>LinkedIn</a></p>
+        <p className='fade-in delay-4'><a href='https://github.com/AdamKuzma' target='_blank' rel='noopener noreferrer'>GitHub</a></p>
+        <p className='fade-in delay-5'><a href='https://www.linkedin.com/in/adamkuzma/' target='_blank' rel='noopener noreferrer'>LinkedIn</a></p>
       </div>
     </div>
   </>
